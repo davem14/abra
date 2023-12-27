fa15a571891d2b4402ac85b3b28eed77c52de594
David Emanuel <david14em@gmail.com>
2023-12-27 15:47:58 +0200

    fix Message model

--- messaging_system/messaging/models.py
+++ messaging_system/messaging/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.SET_NULL)
-    receiver = models.ForeignKey(User, on_delete=models.SET_NULL)
+    sender = models.ForeignKey(User, null=True, related_name='sent_messages', on_delete=models.SET_NULL)
+    receiver = models.ForeignKey(User, null=True, related_name='received_messages', on_delete=models.SET_NULL)
     message = models.TextField()
     subject = models.CharField(max_length=256)
     creation_date = models.DateField(auto_now_add=True)
