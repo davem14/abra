21005a88ff8b52356159842f903197630e8ebed0
David Emanuel <david14em@gmail.com>
2023-12-27 13:03:28 +0200

    create Message model

--- messaging_system/messaging/models.py
+++ messaging_system/messaging/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL)
+    receiver = models.ForeignKey(User, on_delete=models.SET_NULL)
+    message = models.TextField()
+    subject = models.CharField(max_length=256)
+    creation_date = models.DateField(auto_now_add=True)
+    read = models.BooleanField(default=False)
