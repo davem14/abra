3559250517fa1634642dee103d08d1e4c1de7f57
David Emanuel <david14em@gmail.com>
2023-12-27 17:20:51 +0200

    create views classes template

--- messaging_system/messaging/views.py
+++ messaging_system/messaging/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class WriteMessage(APIView):
+
+    def post(self, request):
+        pass
+
+
+class ReadMessage(APIView):
+
+    def get(self, request):
+        pass
+
+
+class ReadAllMessages(APIView):
+
+    def get(self, request):
+        pass
+
+
+class ReadAllUnreadMessages(APIView):
+
+    def get(self, request):
+        pass
+
+
+class DeleteMessage(APIView):
+
+    def delete(self, request):
+        pass
